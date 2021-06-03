978147a518d25968fe09a44e2c516b669670ac1e
justin <justinadams758@gmail.com>
2021-06-03 10:57:58 -0500

    animals can now be deleted from database

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useState } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import "./Animal.css"
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 
 export const AnimalDetail = () => {
-    const { animals } = useContext(AnimalContext)
+    const { animals, releaseAnimal } = useContext(AnimalContext)
     const [ animal, setAnimal ] = useState({ location: {}, customer: {} })
 
  
@@ -16,13 +16,23 @@ export const AnimalDetail = () => {
 
         setAnimal(thisAnimal)
     }, [animalId])
+    
+    const history = useHistory()
 
+const handleRelease = () => {
+    releaseAnimal(animal.id)
+      .then(() => {
+        history.push("/animals")
+      })
+    }
     return (
-    <section className="animal">
+        <section className="animal">
         <h3 className="animal__name">{ animal.name }</h3>
         <div className="animal__breed">{ animal.breed }</div>
         <div className="animal__location">Location: { animal.location.name }</div>
         <div className="animal__owner">Customer: { animal.customer.name }</div>
+        <button onClick={handleRelease}>Release Animal</button>
     </section>
     )
+
 }
--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -24,6 +24,13 @@ export const AnimalProvider = (props) => {
         .then(response => response.json())
     }
 
+    const releaseAnimal = animalId => {
+        return fetch(`http://localhost:8088/animals/${animalId}`, {
+            method: "DELETE"
+        })
+            .then(getAnimals)
+    }
+
     /*
         You return a context provider which has the
         `animals` state, `getAnimals` function,
@@ -35,7 +42,8 @@ export const AnimalProvider = (props) => {
         value={{
             animals, 
             getAnimals, 
-            addAnimal
+            addAnimal,
+            releaseAnimal
         }}>
             {props.children}
         </AnimalContext.Provider>
